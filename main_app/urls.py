0a6cd6f828016355a7055df5c5efa6b07059e22c
adferrin <adferrin@icloud.com>
2020-09-15 15:02:34 -0700

    articles functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('profiles/', views.profiles_index, name='index'),
 
+    path('articles/', views.articles_index, name='articles_index'),
+    path('articles/<int:article_id>/', views.articles_detail, name='articles_detail'),
+    path('articles/create', views.ArticleCreate.as_view(), name='articles_create'),
+    path('articles/<int:pk>/update/', views.ArticleUpdate.as_view(), name='articles_update'),
+    path('articles/<int:pk>/delete/', views.ArticleDelete.as_view(), name='articles_delete'),
+
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DeleteView, ListView 
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Profile, Article
 
@@ -50,3 +53,23 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+def articles_index(request):
+    articles = Article.objects.all()
+    return render(request, 'articles/index.html', { 'articles': articles })
+
+def articles_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'articles/detail.html', { 'article': article })
+
+class ArticleCreate(CreateView):
+    model = Article
+    fields = '__all__'
+
+class ArticleUpdate(UpdateView):
+    model = Article
+    fields = '__all__'
+
+class ArticleDelete(DeleteView):
+    model = Article
+    success_url = '/articles/'
