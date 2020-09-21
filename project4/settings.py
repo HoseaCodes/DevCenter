3c4b841832992c24cc9a9f8d2bf1247da556c6b1
Diego Burgos <diegoburgos@MacBook-Pro.attlocal.net>
2020-09-21 15:47:45 -0500

    Env and Setting.py update3

--- project4/settings.py
+++ project4/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -169,4 +169,3 @@ import django_heroku
 django_heroku.settings(locals())
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
