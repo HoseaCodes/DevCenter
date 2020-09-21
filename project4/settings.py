fdaadf5b51c25e82ca3b23f2f96cdf9371fee13d
Diego Burgos <diegoburgos@MacBook-Pro.attlocal.net>
2020-09-21 17:47:31 -0500

    Env and Setting.py update11

--- project4/settings.py
+++ project4/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
