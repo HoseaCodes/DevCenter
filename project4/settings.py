a6bad33155da11893cafa29c11ee66094b68576d
Diego Burgos <diegoburgos@MacBook-Pro.attlocal.net>
2020-09-21 15:49:16 -0500

    Env and Setting.py update4

--- project4/settings.py
+++ project4/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
