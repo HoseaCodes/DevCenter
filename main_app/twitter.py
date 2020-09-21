c43db6d8bf3271461e782a846f42f68e5756de1a
hosead6168 <dominique11h@yahoo.com>
2020-09-20 19:38:36 -0500

    new branch

--- main_app/twitter.py
+++ main_app/twitter.py
@@ -16,19 +16,22 @@ class TwitterClient():
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
         self.twitter_user = twitter_user
+
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
         for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
             tweets.append(tweet)
         return tweets
+
     def get_friend_list(self, num_friends):
         friend_list = []
-        for firend in Cursor(self.twitter_client.firends).items(num_friends):
+        for firend in Cursor(self.twitter_client.firends, id=self.twitter_user).items(num_friends):
             friend_list.append(firend)
         return friend_list
+
     def get_home_timeline_tweets(self, num_tweets):
         home_timeline_tweets = []
-        for tweet in Cursor(self.twitter_client.home_timeline).items(num_tweets):
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
             home_timeline_tweets.append(tweet)
         return home_timeline_tweets
 
